d48cc8f7e6b69bb75bdcc4c0a62658f1801cead3
Wu Young <doomsplayer@gmail.com>
2015-11-03 21:02:39 +0800

    use deploy script

---
+++ deploy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import subprocess, os
+def run(*args):
+    ret = subprocess.call(args,  stdout=sys.stdout, stderr=sys.stderr)
+    if ret != 0:
+        exit(ret)
+
+token = os.environ['GH_TOKEN']
+repo = os.environ['TRAVIS_REPO_SLUG']
+
+print('uploading site...')
+sys.stdout.flush()
+run('git', 'push', '-fq', 'https://%s@github.com/%s.git' % (token, repo), 'master')
