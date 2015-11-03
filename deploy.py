22b4f33281758e6e8af32c4d270e7a5c9d43a3cb
Wu Young <doomsplayer@gmail.com>
2015-11-03 22:37:48 +0800

    move script to python

--- deploy.py
+++ deploy.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-import subprocess, os, sys
+import subprocess, os, sys, shutil
+from itertools import chain
+
 def run(*args):
     ret = subprocess.call(args,  stdout=sys.stdout, stderr=sys.stderr)
     if ret != 0:
@@ -9,5 +11,26 @@ token = os.environ['GH_TOKEN']
 repo = os.environ['TRAVIS_REPO_SLUG']
 
 print('uploading site...')
-sys.stdout.flush()
+
+print('checkout to master')
+run('git', 'checkout', '--orphan', 'master')
+
+print('clean up directory')
+for (dirpath, dirnames, filenames) in walk("."):
+    for folder in dirnames:
+        if folder not in ['public', '.git']:
+            shutil.rmtree(folder)
+    for filename in filenames:
+        if filename != '.gitignore':
+            shutil.rmtree(filename)
+    break
+for (_, dirnames, filenames) in walk("public"):
+    for item in chain(dirnames, filenames):
+        shutil.move(item, ".")
+    break
+shutil.rmtree('public')
+
+print('push to git')
+run('git', 'add', '-A')
+run('git', 'commit', '-am', "generate content")
 run('git', 'push', '-fq', 'https://%s@github.com/%s.git' % (token, repo), 'master')
